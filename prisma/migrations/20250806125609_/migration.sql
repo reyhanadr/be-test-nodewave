ab8530e020bcd9cf9d33f2a6e24df09b0bfcdb02
reyhanadr <reyhan.a.deris@gmail.com>
2025-08-07 13:41:35 +0700

    fix 1 error deploy

--- prisma/migrations/20250806125609_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `product` ALTER COLUMN `updatedAt` DROP DEFAULT;
--- prisma/seeds/index.ts
+++ prisma/seeds/index.ts
@@ -1,9 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 import { seedAdmin } from "./seedAdmin";
+import { seedProduct } from "./seedProduct";
 
 async function seed(){
     const prisma = new PrismaClient()
     await seedAdmin(prisma)
+    await seedProduct(prisma)
 }
 
 seed().then(()=>{
